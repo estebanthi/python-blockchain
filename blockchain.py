1689e96b06b48026eb2c944da3b796a90f3e85ea
estebanthi <estebanthi62@gmail.com>
2022-07-09 09:54:46 +0200

    Proof of Work

--- blockchain.py
+++ blockchain.py
@@ -13,6 +13,18 @@ class Blockchain(object):
 
         self.new_block(proof=100, previous_hash=1)
 
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     def new_block(self, proof, previous_hash=None):
         index = len(self.chain) + 1
         timestamp = time()
